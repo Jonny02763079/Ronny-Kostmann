1c9e0fb4b427a644c0a785fd8a1dbdc70f431f1e
Ronny <kostmann.b@gmx.at>
2022-04-27 16:04:53 +0200

    Würfelspiel fertig

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -5,8 +5,11 @@ public class Bankomat {
     public static void main(String[] args) {
 
 
-        boolean end = false;
-        int selecionkapital = 0;
+        boolean isFinished = false;
+
+        int balance = 0;
+        int amount = 0;
+
         String ID0 = "hund";
         String ID1 = "Affe";
 
@@ -15,50 +18,52 @@ public class Bankomat {
         Scanner scanner = new Scanner(System.in);
         String selection = scanner.next();
 
-        if (selection.equals(ID0) || selection.equals(ID1)){
-
-            System.out.println("1 Einzahlen");
-            System.out.println("2 Abheben");
-            System.out.println("3 Kontostand");
-            System.out.println("4 Zahlung Beenden");
-
-            while (!end){
+        if (selection.equals(ID0) || selection.equals(ID1)) {
 
 
-                int wahl = scanner.nextInt();
-                int selectionstand = 0;
-                int selectionkapital = 0;
-                int selectionbetrag = 0;
-                if(wahl == 1){
+
+            while (!isFinished) {
+
+                printMenu();
+                int functionSelection = scanner.nextInt();
+
+
+                if (functionSelection == 1) {
                     System.out.println("Wie viel Geld wollen sie einzahlen");
-                    selectionkapital = scanner.nextInt();
-                    System.out.println("Sie haben " + selectionkapital + " Euro eingezahlt");
-                }
-                if(wahl == 2){
-                    System.out.println("Wie viel Geld wollen sie abheben");
-                    selectionstand = scanner.nextInt();
+                    amount = scanner.nextInt();
+                    balance = balance + amount;
+                    System.out.println("Sie haben " + amount + " Euro eingezahlt");
 
-                    System.out.println("Sie haben " + selectionstand + " Euro abgehoben");
-                    if (selectionstand <= selectionkapital){
+                } else if (functionSelection == 2) {
+                    System.out.println("Wie viel Geld wollen sie abheben");
+                    amount = scanner.nextInt();
+                    if (balance <= amount) {
                         System.out.println("Sie besitzen zu wenig Geld");
+                    } else {
+                        System.out.println("Sie haben " + amount + " Euro abgehoben");
+                        balance = balance - amount;
                     }
-                    else {
-                        selectionbetrag = selectionkapital - selectionstand;
-                    };
-                }
-                if(wahl == 3){
-                    System.out.println("Ihr Kontostand beträgt " + selectionbetrag + " Euro");
-                }
-                if(wahl == 4){
+
+                } else if (functionSelection == 3) {
+                    System.out.println("Ihr Kontostand beträgt " + balance + " Euro");
+
+                } else if (functionSelection == 4) {
                     System.out.println("Kontoeinzug Beendet");
-                    end = true;
+                    isFinished = true;
                     break;
                 }
 
-        }
-    }else{
+            }
+        } else {
             System.out.println("Passwort falsch");
             return;
         }
-}
+    }
+
+    public static void printMenu() {
+        System.out.println("1 Einzahlen");
+        System.out.println("2 Abheben");
+        System.out.println("3 Kontostand");
+        System.out.println("4 Zahlung Beenden");
+    }
 }
---
+++ src/Würfelspiel.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Würfelspiel {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Press p to play");
+
+        Scanner scanner = new Scanner(System.in);
+        String selection = scanner.next();
+
+
+        int player = 0;
+        int computer = 0;
+        String IdStart = "p";
+
+
+        if (selection.equals(IdStart)) {
+
+
+            int trys = 5;
+            int playerbalance = 0;
+            int computerbalance =0;
+
+            while (trys >= 0) {
+
+
+                trys = trys - 1;
+
+                int min = 1;
+                int max = 5;
+
+                Random random = new Random();
+
+                System.out.println("Spieler würfelt");
+
+                int value = random.nextInt(max + min) + min;
+                System.out.println(value);
+
+                playerbalance = playerbalance + value;
+
+                System.out.println("Computer würfelt");
+
+                int valueComputer = random.nextInt(max + min) + min;
+                System.out.println(valueComputer);
+
+                computerbalance = computerbalance + valueComputer;
+
+
+            }
+            if(playerbalance > computerbalance){
+                System.out.println("Sie haben gewonnen mit einer Gesammtsumme von " + playerbalance);
+                System.out.println("Der Computer hat " + computerbalance);
+            } else if (computerbalance > playerbalance){
+                System.out.println("Der Computer hat gewonnen mit einer Gesammtsumme von " + computerbalance);
+                System.out.println("Sie haben " + playerbalance);
+            } else if (playerbalance == computerbalance){
+                System.out.println("Es ist unentschieden, sie haben beide eine Gesammtsumme von " + playerbalance);
+            }
+        }
+    }
+}
